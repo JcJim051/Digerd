ab5e38519b504ee09f07b9c4879ecc5f5bfaae0c
juanfajardobaires <juan.fajardo@bairesdev.com>
2021-05-26 12:48:08 -0500

    api created

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post("/login",'App\Http\Controllers\ApiAuthController@login')->name('apilogin');
+Route::get("/municipios",'App\Http\Controllers\ApiController@municipios')->name('apimunicipios');
