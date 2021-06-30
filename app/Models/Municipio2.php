5e67670364c48923a518b1c14e447edb58215ba1
juanfajardobaires <juan.fajardo@bairesdev.com>
2021-06-30 17:09:19 -0500

    municipio2 for api

---
+++ app/Models/Municipio2.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Class Municipio
+ * 
+ * @property int $id_municipio
+ * @property character varying|null $nombre
+ * 
+ * @property Collection|Emergencia[] $emergencias
+ *
+ * @package App\Models
+ */
+class Municipio extends Model
+{
+	protected $table = 'municipios';
+	protected $primaryKey = 'id_municipio';
+	public $incrementing = false;
+	public $timestamps = false;
+	
+
+	protected $fillable = [
+		'nombre'
+	];
+
+	public function emergencias()
+	{
+		return $this->hasMany(Emergencia::class, 'id_municipio');
+	}
+}
