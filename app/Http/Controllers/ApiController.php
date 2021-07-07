3631474dfdbb1af127c54bfcd5e46572fcab906f
juanfajardobaires <juan.fajardo@bairesdev.com>
2021-07-07 11:59:30 -0500

    api fixed

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Municipio2;
+use App\Models\TiposEmergencia;
+use Illuminate\Support\Facades\Storage;
 class ApiController extends Controller
 {
     //
     public function municipios()
     {
         return Municipio2::Select("id_municipio","nombre")->get();
+    }
+    public function tiposemergencia()
+    {
+        return TiposEmergencia::all();
+    }
+    public function postemergencia(Request $request)
+    {
+        Storage::put('data.txt',$request->getContent());
     }
 }
