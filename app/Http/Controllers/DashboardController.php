22640fcb958fdf3a2e6f56792dcc765c2634d688
juanfajardobaires <juan.fajardo@bairesdev.com>
2021-07-20 22:51:29 -0500

    added city selection to maps

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Actividad;
 use App\Models\Generic;
+use App\Models\Municipio2;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
@@ -25,9 +26,12 @@ class DashboardController extends Controller
         ON (pa.id_emergencia=e.id_emergencia )
         group by e.descripcion,e.id_emergencia,e.localizacion ";
         $result=DB::Select($sql);
+        $municipios=Municipio2::all();
         $puntos=Generic::Hydrate($result)->all();  
-      $actividades=Actividad::where("id_funcionario","=",auth()->user()->id_funcionario)->where("cumplida","=",0)->orderBy("fecha_estimada")->get();  
-      return view("vendor.voyager.index",compact('actividades','puntos'));
+      
+      
+        $actividades=Actividad::where("id_funcionario","=",auth()->user()->id_funcionario)->where("cumplida","=",0)->orderBy("fecha_estimada")->get();  
+      return view("vendor.voyager.index",compact('actividades','puntos','municipios'));
     }
    
 }
