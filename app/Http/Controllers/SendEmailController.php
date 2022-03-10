bbfe129a2db02078c08368399a0173614724f84f
CandidoM <stiivenmoreno@gmail.com>
2022-03-09 23:56:19 -0500

    Modificacion api para llenar funcionario y PQRSD tabla respuesta

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -10,15 +10,16 @@ use Illuminate\Support\Facades\Mail;
 
 class SendEmailController extends Controller
 {
-    public function responsable(Request $request){
+    public function responsable(Request $request)
+    {
         $responsable = Funcionario::find($request->responsable);
         //dd($responsable);
         $details = [
-            'title' => $request->tipo.': Nueva correspondencia PQRSD',
-            'subtitle' => 'Sr(a). '.$responsable->nombre.'. Se le ha asignado una nueva correspondencia PQRSD: ' . $request->asunto,
+            'title' => $request->tipo . ': Nueva correspondencia PQRSD',
+            'subtitle' => 'Sr(a). ' . $responsable->nombre . '. Se le ha asignado una nueva correspondencia PQRSD: ' . $request->asunto,
             'body' => 'La información pqrsd suministrada es la siguiente; ' .
-                'Remitente: ' . $request->nombre_remitente . ', Correo remitente: ' . $request->email_remitente.' ',
-            'descripcion' => 'Recuerde que a partir del momento en que se le asignó esta correspondencia, tiene como máximo 15 días para dar una respuesta a la solicitud, atendiendo con el respectivo soporte en la plataforma. ',
+                'Remitente: ' . $request->nombre_remitente . ', Correo remitente: ' . $request->email_remitente . ' ',
+            'descripcion' => 'Recuerde que a partir del momento en que se le asignó esta correspondencia, tiene como máximo 10 días para dar una respuesta a la solicitud, atendiendo con el respectivo soporte en la plataforma. ',
             'button' => 'Ver correspondencia',
             'enlace' => url('/respuestaPQRSD')
         ];
