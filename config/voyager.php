ac71f61e687845928a2a981e40aaa989c297f430
Jonathan <jonathan.c.jimenez@gmail.com>
2024-12-03 12:24:02 -0500

    Modulo contratos

--- config/voyager.php
+++ config/voyager.php
@@ -194,7 +194,7 @@ return [
 
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
-        //'css/custom.css',
+        'css/custom.css',
         'wave/css/admin.css',
         'fa/css/font-awesome.min.css'
     ],
